44b476731b06855ddbfd0bd34d1d6609cdde2767
viniciuslalli <viniciuslalli@gmail.com>
2025-01-22 21:17:31 -0300

    feat: add resiliece to RabbitMQ notification service

--- src/main/java/com/lalli/proposta_app/service/NotificarRabbitMQService.java
+++ src/main/java/com/lalli/proposta_app/service/NotificarRabbitMQService.java
@@ -1,6 +1,7 @@
 package com.lalli.proposta_app.service;
 
 import com.lalli.proposta_app.dto.PropostaResponseDTO;
+import com.lalli.proposta_app.entity.Proposta;
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ public class NotificarRabbitMQService {
 
     private RabbitTemplate rabbitTemplate;
 
-    public void notificar(PropostaResponseDTO proposta, String exchange) {
+    public void notificar(Proposta proposta, String exchange) {
         rabbitTemplate.convertAndSend(exchange,"", proposta);
 
     }
--- src/main/java/com/lalli/proposta_app/service/PropostaService.java
+++ src/main/java/com/lalli/proposta_app/service/PropostaService.java
@@ -5,8 +5,6 @@ import com.lalli.proposta_app.dto.PropostaResponseDTO;
 import com.lalli.proposta_app.entity.Proposta;
 import com.lalli.proposta_app.mapper.PropostaMapper;
 import com.lalli.proposta_app.repository.PropostaRepository;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -33,11 +31,23 @@ public class PropostaService {
         Proposta proposta = PropostaMapper.INSTANCE.convertDtoToProposta(requestDTO);
         propostaRepository.save(proposta);
 
-        PropostaResponseDTO response = PropostaMapper.INSTANCE.convertEntityToDto(proposta);
-        notificarRabbitMQService.notificar(response, exchange);
+        notificarRabbitMQ(proposta);
 
-        return response;
+        notificarRabbitMQService.notificar(proposta, exchange);
+
+        return PropostaMapper.INSTANCE.convertEntityToDto(proposta);
     }
+
+    private void notificarRabbitMQ(Proposta proposta) {
+        try {
+            notificarRabbitMQService.notificar(proposta, exchange);
+        } catch (RuntimeException e) {
+            // Caso houver indisponibilidade do serviço de mensageria, a proposta não será integrada
+            proposta.setIntegrada(false);
+            propostaRepository.save(proposta);
+        }
+    }
+
 
     public List<PropostaResponseDTO> obterProposta() {
        return  PropostaMapper.INSTANCE.convertListEntityToListDto(propostaRepository.findAll());
